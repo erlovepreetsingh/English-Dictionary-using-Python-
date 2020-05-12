1b37bc29e6398df9e65496221d392633254a9479
Lovepreet Singh <64769433+erlovepreetsingh@users.noreply.github.com>
2020-05-12 12:22:21 +0530

    modified

--- dictionary_finder_sqlConnect.py
+++ dictionary_finder_sqlConnect.py
@@ -1,58 +1,63 @@
 import mysql.connector
 from difflib import get_close_matches
 
-con = mysql.connector.connect(
-user = "ardit700_student",
-password = "ardit700_student",
-host = "108.167.140.122",
-database = "ardit700_pm1database",
-)
+def loopagain():
+    con = mysql.connector.connect(
+    user = "ardit700_student",
+    password = "ardit700_student",
+    host = "108.167.140.122",
+    database = "ardit700_pm1database",
+    )
 
-cursor = con.cursor()
+    cursor = con.cursor()
 
-def get_expressions():
-    cursor.execute("SELECT * FROM Dictionary")
-    results = cursor.fetchall()
+    def get_expressions():
+        cursor.execute("SELECT * FROM Dictionary")
+        results = cursor.fetchall()
 
-    exprs = []
+        exprs = []
 
-    for items in results:
-        expression = items[0]
-        exprs.append(expression)
-    return exprs
+        for items in results:
+            expression = items[0]
+            exprs.append(expression)
+        return exprs
 
-def meaning(term):
+    def meaning(term):
      
-    if term in list_of_expressions:
-        cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % term)
-        return cursor.fetchall()
-
-
-
-    elif len(get_close_matches(term, list_of_expressions)) > 0:
-        response =  input("Did you mean %s instead ? Enter Y if yes, or N if no: "% get_close_matches(term, list_of_expressions)[0])
-        if response == "Y":
-            word_to_find = get_close_matches(term, list_of_expressions)[0]
-            cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % word_to_find)
+        if term in list_of_expressions:
+            cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % term)
             return cursor.fetchall()
+
+
+
+        elif len(get_close_matches(term, list_of_expressions)) > 0:
+            response =  input("Did you mean %s instead ? Enter Y if yes, or N if no: "% get_close_matches(term, list_of_expressions)[0])
+            if response == "Y":
+                word_to_find = get_close_matches(term, list_of_expressions)[0]
+                cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % word_to_find)
+                return cursor.fetchall()
              
-        elif response == "N" :
-            return "Please double check your word."
-        else:
-            return "We did't understand your response."
+            elif response == "N" :
+                return "Please double check your word."
+            else:
+                return "We did't understand your response."
         
 
-    elif len(get_close_matches(term, list_of_expressions)) == 0:
-        return "This word does not exsists.Check your word. "
+        elif len(get_close_matches(term, list_of_expressions)) == 0:
+            return "This word does not exsists.Check your word. "
 
 
 
-word = input("Enter the word: ")
-list_of_expressions = get_expressions()
-word = word.lower()
-output = meaning(word)
-if type(output) == list:
-    for item in output:
-        print("Meaning - " + item[1])
-else:
-    print(output)
+    word = input("Enter the word: ")
+    list_of_expressions = get_expressions()
+    word = word.lower()
+    output = meaning(word)
+    if type(output) == list:
+        for item in output:
+            print("Meaning - " + item[1])
+    else:
+        print(output)
+    print("hello")    
+    loopagain()
+
+loopagain()
--- python_word_dict.py
+++ python_word_dict.py
@@ -1,29 +1,37 @@
+
 import json
 from difflib import get_close_matches
-data = json.load(open('data.json'))
 
-def meaning(term):
+def loopagain():
+
+    data = json.load(open('data.json'))
+
+    def meaning(term):
      
-    if term in data:
-        return data[term]
-    elif len(get_close_matches(term, data.keys())) > 0:
-        response =  input("Did you mean %s instead ? Enter Y if yes, or N if no: "% get_close_matches(term, data.keys())[0])
-        if response == "Y":
-            return data[get_close_matches(term, data.keys())[0]] 
-        elif response == "N" :
-            return "Please double check your word."
-        else:
-            return "We did't understand your response."
+        if term in data:
+            return data[term]
+        elif len(get_close_matches(term, data.keys())) > 0:
+            response =  input("Did you mean %s instead ? Enter Y if yes, or N if no: "% get_close_matches(term, data.keys())[0])
+            if response == "Y":
+                return data[get_close_matches(term, data.keys())[0]] 
+            elif response == "N" :
+                return "Please double check your word."
+            else:
+                return "We did't understand your response."
         
 
-    else:
-        return "This word does not exsists.Check your word. "
+        else:
+            return "This word does not exsists.Check your word. "
 
-word = input("Enter the word: ")
-word = word.lower()
-output = meaning(word)
-if type(output) == list:
-    for items in output:
-        print(items)
-else:
-    print(output)
+    word = input("Enter the word: ")
+    word = word.lower()
+    output = meaning(word)
+    if type(output) == list:
+        for items in output:
+            print(items)
+    else:
+        print(output)
+    loopagain()
+
+loopagain()
+
